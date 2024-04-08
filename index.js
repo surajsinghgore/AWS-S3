ce7f367737bf87caa966a1e325de98029ed0c336
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-04-08 16:05:24 +0530

    dotenv file added

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const dotenv=require('dotenv');
 
-
+dotenv.config({});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log(process.env.AWS_ACCESSKEYID);
+    res.send('Hello World!')
 })
+
+// getting particular filename from s3 bucket in private acces
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
